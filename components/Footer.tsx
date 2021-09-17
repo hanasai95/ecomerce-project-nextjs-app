7ae749dd08b4c3009597a0f81da21290405f307a
hanasai95 <1220hanasai@gmail.com>
2021-09-16 23:58:51 -0400

    added footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,16 @@
+import styles from '../styles/Footer.module.scss'
 
 export default function Footer() {
     return (
-        <footer>
-            footer
-        </footer>
+        <footer className={styles.footer}>
+            <p>
+                © Hana Cai 2021
+            </p>
+            <div className={styles.footer__left}>
+                <a href="https://github.com/hanasai95">Github</a> <br />
+                <a href="https://www.linkedin.com/in/hana-c-b43a32211/">LinkedIn</a>
+
+            </div>
+        </footer >
     )
 }
